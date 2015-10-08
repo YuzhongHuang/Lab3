910182108156624ee4bac787a07b4c3c31a9046c
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-10-07 20:07:24 -0400

    add alertdialog to warn user when skiping challenges

--- ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/Fragments/ClueFetch.java
+++ ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/Fragments/ClueFetch.java
@@ -1,6 +1,8 @@
 package com.example.yhuang.scavengerhunt.Fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -51,6 +53,22 @@ public class ClueFetch extends Fragment {
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+                alertDialogBuilder.setMessage(R.string.clue_switch_message);
+
+                alertDialogBuilder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //nothing
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton(R.string.go_back, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //nothing
+                    }
+                });
 
             }
         });
