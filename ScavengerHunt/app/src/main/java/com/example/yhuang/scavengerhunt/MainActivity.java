37d4735531636a5bd6a43c025eee7ed3577575d9
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-10-11 09:30:01 -0400

    deletes empty lines

--- ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/MainActivity.java
+++ ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.yhuang.scavengerhunt;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-
 import com.example.yhuang.scavengerhunt.Fragments.ClueFetch;
 import com.example.yhuang.scavengerhunt.Fragments.GpsDetection;
 import com.example.yhuang.scavengerhunt.Fragments.ImageUpload;
