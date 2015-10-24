07f04175eeb4fb1cb76b6053ec9c7c7281a784a3
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-10-24 12:04:30 -0400

    saving image to local storage and go back to main after taking the image

--- ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/CameraActivity.java
+++ ScavengerHunt/app/src/main/java/com/example/yhuang/scavengerhunt/CameraActivity.java
@@ -1,17 +1,20 @@
 package com.example.yhuang.scavengerhunt;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle;   
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
 
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
+    private final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +32,23 @@ public class CameraActivity extends AppCompatActivity {
      */
     public void onLaunchCamera(View view) {
         // create an implicit Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException err) {
+                // Error occurred while creating the File
+                err.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
         }
     }
 
@@ -45,16 +62,28 @@ public class CameraActivity extends AppCompatActivity {
     */
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode != RESULT_CANCELED && resultCode == RESULT_OK) {
-                    Bundle extras = data.getExtras();
-                    Bitmap takenImage = (Bitmap) extras.get("data");
-
-                    //uploadImageToDatabase(Bitmap takenImage);
-
-                    ImageView ivPreview = (ImageView) findViewById(R.id.imageView_test);
-                    ivPreview.setImageBitmap(takenImage);
+            Toast.makeText(this, mCurrentPhotoPath, Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, R.string.no_picture, Toast.LENGTH_SHORT).show();
         }
+
+        Intent MainActivity = new Intent(this, MainActivity.class);
+        this.startActivity(MainActivity);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "TakenImage";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
 }
